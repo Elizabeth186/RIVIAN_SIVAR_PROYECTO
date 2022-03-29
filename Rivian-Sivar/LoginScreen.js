f0519a2c210f9186c0ec340c15c2e684c92e9286
Elizabeth186 <eliizabethsuarezz@gmail.com>
2022-03-29 13:48:51 -0600

    diseño final del login

--- Rivian-Sivar/LoginScreen.js
+++ Rivian-Sivar/LoginScreen.js
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/core'
 import React, { useEffect, useState } from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text,
+   TextInput, TouchableOpacity, View, Image } from 'react-native'
 import { auth } from './firebase'
 
 const LoginScreen = () => {
@@ -38,9 +39,20 @@ const LoginScreen = () => {
   }
 
   return (
+    
     <KeyboardAvoidingView
-      style={styles.container}
-      behavior="padding">
+        style={styles.container}
+        keyboardVerticalOffset={-100000}>
+     <View
+         style={styles.Viewimg}>        
+       <Image style={styles.ImgPrincipal}
+              source={require("./assets/LoginImg.png")  }/>
+     </View>
+       <View style={styles.viewEmpresa}>
+          <Text style={styles.textRivian}>RIVIAN </Text>
+          <Text style={styles.textSivar}>SIVAR</Text>
+      </View>
+          <Text style={styles.textBienvenida}>BIENVENIDO</Text>
       <View style={styles.View1}>
         <TextInput
           placeholder="Correo Electronico"
@@ -63,16 +75,27 @@ const LoginScreen = () => {
           onPress={Login}
           style={styles.button1}
         >
-          <Text style={styles.buttonText1}>Login</Text>
+          <Text style={styles.buttonText1}>Iniciar Sesion</Text>
         </TouchableOpacity>
         </View>
         <View style={styles.Viewbtn2}>
         <TouchableOpacity
           onPress={SignUp}
           style={[styles.button2]}>
-          <Text style={styles.buttonText2}>Register</Text>
+          <Text style={styles.buttonText2}>Registrarme</Text>
         </TouchableOpacity>
       </View>
+        <View style={styles.Viewbtn3}>
+        <TouchableOpacity
+          onPress={()=>{}}
+          style={[styles.button3]}>
+
+            <Image style={styles.img}
+            source={require("./assets/search.png")  }/>
+          <Text  style={styles.buttonText3}>Google</Text>
+        </TouchableOpacity>
+      </View>
+      
     </KeyboardAvoidingView>
   )
 }
@@ -82,38 +105,129 @@ export default LoginScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    marginTop:40,
     alignItems: 'center',
+    backgroundColor: '#ffffff'
   },
  View1:{
-
+width: 300,
+height: 50,
+marginBottom: 20
  },
  View2:{
+  width: 300,
+  height: 50,
 
  },
  input1:{
-
+ borderColor: ('#307B8C'),
+ borderWidth: 1.5,
+textAlign: 'center',
+height: 50,
+borderRadius: 15,
+fontSize: 15
  },
  input2:{
-
+  borderColor: ('#307B8C'),
+  borderWidth: 1.5,
+ textAlign: 'center',
+ height: 50,
+ borderRadius: 15,
+ fontSize: 15
  },
  Viewbtn1:{
-
+marginTop: 40,
+width:300
  },
- Viewbtn2:{
-     
- },
+ 
  button1:{
+backgroundColor: ('#0E4C59'),
+width: 300,
+height: 50,
+borderRadius: 11,
 
  },
  button2:{
+  backgroundColor: ('#ffffff'),
+  width: 300,
+  height: 50,
+  borderRadius: 11,
+  marginTop:20,
+  borderColor:('#0E4C59'),
+  borderWidth: 1,
 
  },
  buttonText1:{
-
+textAlign: 'center',
+padding:10,
+color:'white',
+fontSize: 17,
  },
  buttonText2:{
-     
- }
+  textAlign: 'center',
+  padding:10,
+  color:('#0E4C59'),
+  fontSize: 17,
+  },
+ Viewbtn3:{
+   backgroundColor:'white',
+   marginTop: 30,
+   height:50,
+   borderRadius: 15
+
+ },
+ buttonText3:{
+  textAlign: 'center',
+  padding:9,
+  fontSize: 17,
+  color:('#0E4C59'),
+  width: 149,
+  backgroundColor:'#ffffff',
+  borderBottomRightRadius: 15,
+  borderTopRightRadius: 15
+  
+ },
+ button3:{
+flexDirection:'row',
+width:300,
+backgroundColor: '#ffffff',
+borderRadius: 11,
+borderColor:'black',
+borderWidth: 1,
+height:50
+ },
+   img:{
+marginTop: 7,
+width:'11%',
+height: 33,
+marginLeft:40,
+  },
+  Viewimg:{
+    width:'100%',
+     height:'30%',
+    alignItems:'center'
+  },
+  ImgPrincipal:{
+    width:'100%',
+     height:'100%'
+  },
+viewEmpresa:{
+flexDirection:'row'
+},
+  textRivian:{
+fontSize:30,
+
+  },
+  textSivar:{
+fontSize: 22,
+padding:8,
+
+  },
+  textBienvenida:{
+    fontSize: 20,
+    marginBottom: 25,
+  
+  }
+ 
 
 })
